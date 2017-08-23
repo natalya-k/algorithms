c6d39e5816caebf39cc3b022d3d47b71ceca37bb
natalya-k <nat-kasitsky@yandex.ru>
2017-08-23 10:18:24 +0300

    Add files via upload

---
+++ sort/MergeSort.cs
@@ -0,0 +1,76 @@
+﻿namespace Algorithms
+{
+    static class MergeSort
+    {
+        public static void Run(int[] array)
+        {
+            Sort(array, 0, array.Length - 1);
+        }
+
+        private static void Sort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                //int middle = (left + right) / 2;
+                int middle = left + (right - left) / 2; //этот вариант предотвращает переполнение стека при больших значениях left, right
+
+                Sort(array, left, middle);
+                Sort(array, middle + 1, right);
+
+                Merge(array, left, middle, right);
+            }
+        }
+
+        private static void Merge(int[] array, int left, int middle, int right)
+        {
+            int leftLength = middle - left + 1;
+            int rightLength = right - middle;
+
+            int[] leftHalf = new int[leftLength];
+            int[] rightHalf = new int[rightLength];
+
+            int i, j, k = left;
+
+            //tbd переделать for в while?
+
+            for (i = 0; k <= middle; i++, k++)
+            {
+                leftHalf[i] = array[k];
+            }
+
+            for (j = 0; k <= right; j++, k++)
+            {
+                rightHalf[j] = array[k];
+            }
+            
+            i = j = 0;
+            k = left;
+
+            while (i < leftHalf.Length && j < rightHalf.Length)
+            {
+                if (leftHalf[i] < rightHalf[j])
+                {
+                    array[k] = leftHalf[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = rightHalf[j];
+                    j++;
+                }
+
+                k++;
+            }
+
+            for (; i < leftHalf.Length; i++, k++)
+            {
+                array[k] = leftHalf[i];
+            }
+
+            for (; j < rightHalf.Length; j++, k++)
+            {
+                array[k] = rightHalf[j];
+            }
+        }
+    }
+}
---
+++ sort/QuickSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    static class QuickSort
+    {
+        public static void Run(int[] array)
+        {
+            Sort(array, 0, array.Length - 1);
+        }
+
+        private static void Sort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = 0;
+
+                Divide(array, left, right, ref pivot);
+
+                Sort(array, left, pivot - 1);
+                Sort(array, pivot + 1, right);
+            }
+        }
+
+        private static void Divide(int[] array, int left, int right, ref int pivot)
+        {
+            int k = left;
+
+            for (int i = left; i < right; i++)
+            {
+                if (array[i] <= array[right])
+                {
+                    Swap(array, k, i);
+                    k++;
+                }
+            }
+
+            Swap(array, k, right);
+
+            pivot = k;
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
