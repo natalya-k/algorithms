5070b6ce2969f1988934815c83365aa78ed167ac
natalya-k <nat-kasitsky@yandex.ru>
2017-08-23 10:16:14 +0300

    Create BinarySearch.cs

---
+++ search/BinarySearch.cs
@@ -0,0 +1,35 @@
+namespace Algorithms
+{
+    class BinarySearch
+    {
+        public static int? Search(int[] array, int target)
+        {
+            int begin = 0;
+            int end = array.Length - 1;
+            int guess;
+
+            while (end >= begin)
+            {
+                //guess = (begin + end) / 2;
+
+                //этот вариант предотвращает переполнение стека при больших значениях begin и end
+                guess = begin + (end - begin) / 2;  
+
+                if (array[guess] == target)
+                {
+                    return guess;
+                }
+                else if (array[guess] > target)
+                {
+                    end = guess - 1;
+                }
+                else
+                {
+                    begin = guess + 1;
+                }
+            }
+
+            return null;
+        }
+    }
+}
