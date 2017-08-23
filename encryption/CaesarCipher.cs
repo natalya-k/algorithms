e1365194533a03c068ad46cc28886cd039660d3f
natalya-k <nat-kasitsky@yandex.ru>
2017-08-23 10:19:30 +0300

    Create CaesarCipher.cs

---
+++ encryption/CaesarCipher.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Text;
+
+namespace Algorithms
+{
+    static class CaesarCipher
+    {
+        private const int LettersNumber = 26;
+        private const int Shift = 3;
+        private const string PlainText = "The quick brown fox jumps over the lazy dog.";
+
+        public static void Test()
+        {
+            Console.WriteLine("Plain: {0}", PlainText);
+
+            string encrypted = Encrypt(PlainText, Shift);
+            Console.WriteLine("Encrypted: {0}", encrypted);
+
+            string decrypted = Decrypt(encrypted, Shift);
+            Console.WriteLine("Decrypted: {0}", decrypted);
+        }
+
+        private static string Encrypt(string input, int shift)
+        {
+            StringBuilder resultSB = new StringBuilder();
+
+            foreach (char ch in input)
+            {
+                if (char.IsLetter(ch))
+                {
+                    char offset = char.IsUpper(ch) ? 'A' : 'a';
+
+                    char shifted = (char)((ch + shift - offset) % LettersNumber + offset);
+
+                    resultSB.Append(shifted);
+                }
+                else
+                {
+                    resultSB.Append(ch);
+                }
+            }
+
+            return resultSB.ToString();
+        }
+
+        private static string Decrypt(string input, int shift)
+        {
+            return Encrypt(input, LettersNumber - shift);
+        }        
+    }
+}
